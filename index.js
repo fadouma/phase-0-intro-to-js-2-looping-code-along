bee62ccb24af764cd45e2695d8a720fcd1178c37
fadouma ahmed <fannyahmed85@gmail.com>
2024-07-03 12:10:41 -0600

    complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+function writeCards(name){
+ const arraymessages = []
+ for (let i = 0; i < name.length; i++)
+    {arraymessages .push(`Thank you, ${name[i]}, for the wonderful surprise gift!`)}
+ return arraymessages
+}
+function countDown(number){
+while (number > -1) {
+    console.log(number--)
+}
+}  
